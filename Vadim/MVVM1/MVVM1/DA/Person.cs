57c6fbb5de4c341af151a4917d0b9a92135717a0
cornered13 <91338479+cornered13@users.noreply.github.com>
2022-03-21 19:58:18 +0300

    Add files via upload

---
+++ Vadim/MVVM1/MVVM1/DA/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM1.DA
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public static Person[] GetPersons()   
+        {
+            var result = new Person[]
+            {
+                new Person() { Name ="Альбек", FirstName ="Арберта"},
+                new Person() { Name ="Буракидзе", FirstName ="Корт"},
+                new Person() { Name ="Мадрик", FirstName ="Карин"}
+            };
+            return result;
+        }
+            
+    }
+}
---
+++ Vadim/MVVM1/MVVM1/DA/PersonViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+using System.Windows.Data;
+
+namespace MVVM1.DA
+{
+    internal class PersonViewModel : DependencyObject
+    {
+
+
+        public string FilterText    
+        {
+            get { return (string)GetValue(FilterTextProperty); }
+            set { SetValue(FilterTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty FilterTextProperty
+            =
+            DependencyProperty.Register("FilterText ", typeof(string), typeof(PersonViewModel), new PropertyMetadata(""));
+
+
+        public ICollectionView Items
+        {
+            get { return (ICollectionView)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ICollectionView), typeof(PersonViewModel), new PropertyMetadata(null));
+
+        public PersonViewModel()
+        {
+            Items = CollectionViewSource.GetDefaultView(Person.GetPersons());
+        }
+
+    }
+}
